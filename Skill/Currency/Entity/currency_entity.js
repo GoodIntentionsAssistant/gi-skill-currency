d8b1ca87758d51f487ea2a117132ce4077aa3664
Darren Moore <darren.m@firecreekweb.com>
2019-01-06 22:15:29 +0700

    Entity update paths

--- Skill/Currency/Entity/currency_entity.js
+++ Skill/Currency/Entity/currency_entity.js
@@ -1,15 +1,16 @@
 /**
  * Currency Entity
  */
-var Entity = require('../../../../src/Entity/entity');
-var _ = require('underscore');
+const Entity = girequire('src/Entity/entity');
+
+const _ = require('underscore');
 
 module.exports = class CurrencyEntity extends Entity {
 
 	setup() {
 		this.name = "Currency";
 		this.import = {
-			file: "Data.Common.currencies",
+			file: "Data.Basics.currencies",
 			type: "csv"
 		};
 	}
